87d465a549410ad5bb68c709839fef732ee1d35f
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-10-05 10:40:37 -0300

    tasa de interes promedio
    Imiplementacion del endpoint "Tasa de interes promedio". Ademas, se adjunto los codigos que aparecen en el metodo get_tip

--- cmf/indicadores_financieros_api/indicadores_financieros.py
+++ cmf/indicadores_financieros_api/indicadores_financieros.py
@@ -13,6 +13,10 @@ class IndicadoresFinancierosChilenos(RequestHandler):
     def __init__(self, api_key:str, timeout:str):
         super().__init__(api_key, timeout)
         
+    # ----------------------------------------
+    # Metodos de ayuda para el manejo de casos
+    # ----------------------------------------
+        
     def __validador_fechas(self, dates_dict, freq:str='D'):
         
         # Condiciones logicas
@@ -96,6 +100,10 @@ class IndicadoresFinancierosChilenos(RequestHandler):
                 dates_dict_['to_'][:4],
                 dates_dict_['to_'][5:7]
                 )
+    
+    # ----------------------------------------
+    # Metodos para solicitar datos a la API
+    # ----------------------------------------
         
     def get_dolares(self, codigo:str='Dolares', **query_params):        
         query_params_ = self.__validador_fechas(query_params)     
@@ -111,7 +119,7 @@ class IndicadoresFinancierosChilenos(RequestHandler):
             "https://api.cmfchile.cl/api-sbifv3/recursos_api/euro/periodo/{}/{}/dias_i/{}/{}/{}/dias_f/{}",
             query_params_
             )
-        return super().handle_request(self.endpoint, query_params, codigo)
+        return super().handle_request(self.endpoint, query_params_, codigo)
     
     def get_ipc(self, codigo:str='IPCs', **query_params):
         query_params_ = self.__validador_fechas(query_params, freq='M')
@@ -120,4 +128,60 @@ class IndicadoresFinancierosChilenos(RequestHandler):
             query_params_,
             freq='M'
             )
-        return super().handle_request(self.endpoint, query_params, codigo)
+        return super().handle_request(self.endpoint, query_params_, codigo)
+    
+    def get_tip(self, codigo:str='TIPs', **query_params):
+        query_params_ = self.__validador_fechas(query_params, freq='M')
+        self.endpoint = self.__endpoint_builder(
+            "https://api.cmfchile.cl/api-sbifv3/recursos_api/tip/periodo/{}/{}/{}/{}",
+            query_params_,
+            freq='M'
+            )
+        return super().handle_request(self.endpoint, query_params_, codigo)
+    
+    def get_codigos_tip(self):
+        codigos_tip = {
+            '1':'Operaciones no reajustables en moneda nacional',
+            '2':'Operaciones no reajustables en moneda nacional de menos de 90 días - de menos de 90 días',
+            '3':'Operaciones no reajustables en moneda nacional 90 días o más - de 90 días o más',
+            '4':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 100 unidades de fomento',
+            '5':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 200 unidades de fomento y superiores al equivalente de 100',
+            '6':'Operaciones no reajustables en moneda nacional 90 días o más - Superiores al equivalente de 200 unidades de fomento',
+            '7':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 200 unidades de fomento',
+            '8':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 5.000 unidades de fomento y superiores al equivalente de 200',
+            '9':'Operaciones no reajustables en moneda nacional 90 días o más - Superiores al equivalente de 5.000 unidades de fomento',
+            '10':'Operaciones no reajustable en moneda nacional menores de 90 días - Inferiores al equivalente de 5.000 unidades de fomento',
+            '11':'Operaciones no reajustable en moneda nacional menores de 90 días - Superiores al equivalente de 5.000 unidades de fomento',
+            '12':'Operaciones reajustables en moneda nacional Menores a un año',
+            '13':'Operaciones reajustables en moneda nacional De un año o más. Inferiores o iguales al equivalente de 2000 unidades de fomento',
+            '14':'Operaciones reajustables en moneda nacional De un año o más. Superiores al equivalente de 2000 unidades de fomento',
+            '15':'Operaciones reajustables en moneda nacional Expresadas en Moneda Extranjera',
+            '20':'Operaciones reajustables en moneda nacional De un año o más.',
+            '21':'Operaciones reajustables en moneda nacional Menores a un año',
+            '22':'Operaciones reajustables en moneda nacional De un año o más. Superiores al equivalente de 2000 unidades de fomento',
+            '23':'Operaciones reajustables en moneda nacional De un año o más',
+            '24':'Operaciones reajustables en moneda nacional De un año o más. Inferiores o iguales al equivalente de 2000 unidades de fomento',
+            '25':'Operaciones no reajustables en moneda nacional de menos de 90 días Superiores al equivalente de 5.000 unidades de fomento',
+            '26':'Operaciones no reajustables en moneda nacional de menos de 90 días Inferiores o iguales al equivalente de 5.000 unidades de fomento',
+            '27':'Operaciones no reajustables en moneda nacional de 90 días o más - Inferiores o iguales al equivalente de 5.000 unidades de fomento y superiores al equivalente de 200 unidades de fomento',
+            '28':'Operaciones no reajustables en moneda nacional de 90 días o más - Inferiores o iguales al equivalente de 100 unidades de fomento',
+            '29':'Operaciones no reajustables en moneda nacional de 90 días o más - Superiores al equivalente de 5.000 unidades de fomento',
+            '30':'Operaciones no reajustables en moneda nacional de 90 días o más - Inferiores o iguales al equivalente de 200 unidades de fomento',
+            '31':'Operaciones no reajustables en moneda nacional de 90 días o más - Inferiores o iguales al equivalente de 200 unidades de fomento y superiores al equivalente de 100 unidades de fomento',
+            '32':'Operaciones no reajustables en moneda nacional de 90 días o más - Superiores al equivalente de 200 unidades de fomento',
+            '33':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 200 unidades de fomento',
+            '34':'Operaciones no reajustables en moneda nacional 90 días o más - Superiores al equivalente de 5.000 unidades de fomento',
+            '35':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 5.000 unidades de fomento y superiores al equivalente de 200 unidades de fomento',
+            '36':'Operaciones expresadas en moneda extranjera',
+            '37':'Operaciones no reajustables en moneda nacional de menos de 90 días',
+            '38':' Operaciones en dólares de EE.UU. o expresadas en moneda extranjera',
+            '39':'Operaciones no reajustables en moneda nacional de 90 días o más',
+            '40':'Operaciones reajustables en moneda nacional',
+            '41':'Operaciones expresadas en moneda extranjera Inferiores o iguales al equivalente de 2.000 unidades de fomento',
+            '42':'Operaciones expresadas en moneda extranjera Superiores al equivalente de 2.000 unidades de fomento',
+            '43':'Operaciones cuyo mecanismo de pago consista en la deducción de las respectivas cuotas directamente de la pensión del deudor',
+            '44':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 200 unidades de fomento y superiores al equivalente de 50 unidades de fomento',
+            '45':'Operaciones no reajustables en moneda nacional 90 días o más - Inferiores o iguales al equivalente de 50 unidades de fomento',
+            }
+        
+        return codigos_tip
