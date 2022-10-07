81c89ec605a1fa53f9be0c4c830d46fd4e1b71d0
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-10-07 10:43:38 -0300

    Activos Ponderados por Riesgo de Crédito (IRS) para múltiples períodos

--- cmf/reportes_bancarios_api/reportes_bancarios.py
+++ cmf/reportes_bancarios_api/reportes_bancarios.py
@@ -45,3 +45,11 @@ class ReportesBancariosChilenos(RequestHandler):
             f"/regresionmensual/{query_params['cantidad']}/instituciones/{query_params['instituciones']}/indicadores/patefe"
             )
         return super().handle_request(self.URL_CALL, query_params, codigo)
+    
+    def ac_riesgo_credito(self, **query_params):
+        codigo = 'AdecuacionDeCapital'
+        self.__endpoint_builder(
+            self.ROOT_ADECUACION, 
+            f"/regresionmensual/{query_params['cantidad']}/instituciones/{query_params['instituciones']}/indicadores/irs"
+            )
+        return super().handle_request(self.URL_CALL, query_params, codigo)
